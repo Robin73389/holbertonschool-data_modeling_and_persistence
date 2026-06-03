489f70e58a28a56c464c527deb2ae4a4672d0f48
Robin73389 <12229@holbertonstudents.com>
2026-06-03 02:21:21 +0200

    Add REAL NOT NULL

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE books (
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     genre TEXT NOT NULL,
-    price INTEGER,
-    stock INTEGER,
-    published_year INTEGER,
-)
+    price REAL NOT NULL,
+    stock INTEGER NOT NULL,
+    published_year INTEGER NOT NULL
+);
