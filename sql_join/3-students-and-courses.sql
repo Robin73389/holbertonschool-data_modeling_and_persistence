0f018853031949ebd2e1b753246fff3a33ffb714
Robin73389 <12229@holbertonstudents.com>
2026-06-05 15:32:31 +0200

    For this task, I used the many-to-many relationship and the INNER JOIN to create a relationship between students and courses.

---
+++ sql_join/3-students-and-courses.sql
@@ -0,0 +1,9 @@
+SELECT
+    students.name AS student_name,
+    courses.title AS course_title
+FROM students
+INNER JOIN enrollments
+ON students.id = enrollments.student_id
+INNER JOIN courses
+ON courses.id = enrollments.course_id
+ORDER BY student_name, course_title ASC
